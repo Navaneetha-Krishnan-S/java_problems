5307ec507d5d80d398b2bacd7cf38e08a61b6a22
Navaneetha-Krishnan-S <krish.cgac@gmail.com>
2024-08-01 09:07:56 +0530

    .

--- PrimeorNot.java
+++ PrimeorNot.java
@@ -1,32 +1,34 @@
-//FIND THE N PRIME NUMBER
 
-import java.util.*;
+import java.util.Scanner;
+
 public class PrimeorNot{
-public static void main(String[] args) {
-
-    int n ;
-    System.out.println("Enter a number to find the Prime number: ");
-    Scanner in = new Scanner(System.in);
-
-    n = in.nextInt();
-    in.nextLine();
-    if(n==2){
-        System.out.println(n+" is a Prime Number");
-        
-    }
-    for (int i = 2; i <n; i++) {
-        
-        if(n%i==0){
-            System.out.println(n+ " is not a Prime Number.");
-            break;
-
+    public static void main(String[] args) {
+        System.out.println("Enter a number to find Prime or Not: ");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if(n<=1){
+            System.out.println("Enter number above 2.");
 
         }
-       
-        
-        System.out.println(n+" is a Prime number.");
-        break;
+        if(n==2){
+            System.out.println("2 is a Prime number");
+        }
+    
+        boolean isTrue = true;
+        for (int i = 2; i < n; i++) {
+
+            if(n%i==0){
+                isTrue=false;
+                break;
+            }
+          
+          
+        }
+        if(isTrue==true){
+            System.out.println(n+ " is a Prime Number");
+        }
+        else{
+            System.out.println(n+" is not a Prime Number");
+        }
     }
-    in.close();
-}
 }
